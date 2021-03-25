98a2bfee03bee60462dc5a03e9f1069cadc5c8e5
prashant94157 <0166621m@gmail.com>
2021-03-25 14:07:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import {Cardlist} from './components/card-list/card-list.component';
+import {Search} from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        deployed React
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      monsters : [],
+      searchField:''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}))
+  }
+
+  handleChange = e =>{
+    this.setState({searchField:e.target.value})
+  }
+
+  render() {
+    
+    const { monsters , searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    
+    
+    return (
+      <div className="App">
+        <h1 className='header'>Monsters Rolodex</h1>
+        <Search 
+          placeholder='search monsters' 
+          handler={this.handleChange}
+        />
+        <Cardlist monsters={filteredMonster} />
+      </div>
+    );
+  }
 }
 
 export default App;
